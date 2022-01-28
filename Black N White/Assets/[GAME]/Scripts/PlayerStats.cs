afa60624646e982b79b262ef0ba76b0dce2404a8
Emir Kaan Öz <emirkaanoz261@gmail.com>
2022-01-29 02:10:52 +0300

    ilk

---
+++ Black N White/Assets/[GAME]/Scripts/PlayerStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public const float MaxBoostAmount = 100;
+    public float boostAmount;
+
+}
