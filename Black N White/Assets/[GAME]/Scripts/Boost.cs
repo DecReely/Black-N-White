fd84838653ee56c7ab5641d13a240c4f80194798
Emir Kaan Öz <emirkaanoz261@gmail.com>
2022-01-29 02:55:20 +0300

    boost

--- Black N White/Assets/[GAME]/Scripts/Boost.cs
+++ Black N White/Assets/[GAME]/Scripts/Boost.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject particle;
+    public PlayerStats playerStats;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Pickup(Collider player)
     {
+        Instantiate(particle, transform.position, transform.rotation);
+
+        if (playerStats.boostAmount + playerStats.boostIncreaseAmount < PlayerStats.MaxBoostAmount)
+        {
+            playerStats.boostAmount += playerStats.boostIncreaseAmount;    
+        }
+        else
+        {
+            playerStats.boostAmount = PlayerStats.MaxBoostAmount;
+        }
         
+        Destroy(gameObject);
     }
 }
