46d2dd5c3b4e8838f8385eb040595df539a29f06
Emir Kaan Öz <emirkaanoz261@gmail.com>
2022-01-29 09:13:11 +0300

    shapeshifting bug fixing

--- Black N White/Assets/[GAME]/Scripts/Shapeshifting.cs
+++ Black N White/Assets/[GAME]/Scripts/Shapeshifting.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Shapeshifting : MonoBehaviour
 {
-    private bool alreadyPressed;
+    public bool alreadyPressed;
+    public bool released;
     public PlayerStats playerStats;
     public GameObject player;
     void Update()
     {
 
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && playerStats.boostAmount > 0)
         {
             if (!alreadyPressed)
             { //Buraya boost kısmı eklenecek.
@@ -19,29 +20,46 @@ public class Shapeshifting : MonoBehaviour
                     player.GetComponent<MeshRenderer>().material = playerStats.blackMaterial;
                     playerStats.color = PlayerStats.colorState.black;
                 }
-                if (playerStats.color == PlayerStats.colorState.black)
+                else if (playerStats.color == PlayerStats.colorState.black)
                 {
                     player.GetComponent<MeshRenderer>().material = playerStats.whiteMaterial;
                     playerStats.color = PlayerStats.colorState.white;
                 }
             }
             
+            playerStats.boostAmount -= playerStats.boostDecreaseAmount * Time.deltaTime;
+            Debug.Log(playerStats.boostAmount);
+
+            if (playerStats.boostAmount <= 0)
+            {
+                released = true;
+                Release();
+            }
+                
+
             alreadyPressed = true;
         }
 
-        if (Input.GetKeyUp("space"))
+        if (Input.GetKeyUp("space") && alreadyPressed && !released)
         {
-            alreadyPressed = false;
-            if (playerStats.color == PlayerStats.colorState.white)
-            {
-                player.GetComponent<MeshRenderer>().material = playerStats.blackMaterial;
-                playerStats.color = PlayerStats.colorState.black;
-            }
-            else if (playerStats.color == PlayerStats.colorState.white)
-            {
-                player.GetComponent<MeshRenderer>().material = playerStats.whiteMaterial;
-                playerStats.color = PlayerStats.colorState.black;
-            }
+            Release();
+            released = false;
+        }
+        
+    }
+
+    void Release()
+    {
+        alreadyPressed = false;
+        if (playerStats.color == PlayerStats.colorState.white)
+        {
+            player.GetComponent<MeshRenderer>().material = playerStats.blackMaterial;
+            playerStats.color = PlayerStats.colorState.black;
+        }
+        else if (playerStats.color == PlayerStats.colorState.black)
+        {
+            player.GetComponent<MeshRenderer>().material = playerStats.whiteMaterial;
+            playerStats.color = PlayerStats.colorState.white;
         }
     }
 }
